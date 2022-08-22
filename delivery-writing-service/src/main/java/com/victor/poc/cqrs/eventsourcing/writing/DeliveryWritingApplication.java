9e1f57b733845330807ea1b9f9351d2712cc1eb0
Victor Magalhães <victormagalhaessales@gmail.com>
2022-08-22 17:36:58 -0300

    Criando estrutura básica com módulos maven

---
+++ delivery-writing-service/src/main/java/com/victor/poc/cqrs/eventsourcing/writing/DeliveryWritingApplication.java
@@ -0,0 +1,13 @@
+package com.victor.poc.cqrs.eventsourcing.writing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryWritingApplication {
+
+	public static void main(String[] args) {        
+		SpringApplication.run(DeliveryWritingApplication.class, args);
+	}
+
+}
