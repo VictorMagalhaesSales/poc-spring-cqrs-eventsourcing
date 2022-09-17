cf72d999882d5a2cc1eabd538b04adce373c053f
Victor Magalhães <victormagalhaessales@gmail.com>
2022-09-17 17:44:01 -0300

    Adicionando MongoDB no serviço de leitura

--- delivery-reading-service/src/main/java/com/victor/poc/cqrs/eventsourcing/reading/DeliveryReadingApplication.java
+++ delivery-reading-service/src/main/java/com/victor/poc/cqrs/eventsourcing/reading/DeliveryReadingApplication.java
@@ -1,12 +1,13 @@
 package com.victor.poc.cqrs.eventsourcing.reading;
 
-
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableRabbit
 @SpringBootApplication
+@EnableRabbit
+@EnableMongoRepositories
 public class DeliveryReadingApplication {
 
 	public static void main(String[] args) {        
